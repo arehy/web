18edea4dfdb03f6fd27fdbea19bcc585137967e9
arehy <36758347+arehy@users.noreply.github.com>
2018-03-05 15:53:19 +0100

    táblázat php-van megoldva

---
+++ php/table.php
@@ -0,0 +1,30 @@
+<?php
+  $items = array(
+    "BGMAAAA" => "Gallowfields",
+    "BAYAAAA" => "Halacon",
+    "BBIAAAA" => "Cereboth",
+    "BKsAAAA" => "Demetra",
+    "BJMBAAA" => "Icegate",
+    "BOQAAAA" => "Provern Shore",
+    "BOwAAAA" => "Nentor",
+    "BLQAAAA" => "Lornar's",
+    "BL8AAAA" => "Snowhawk Landing",
+    "BGUCAAA" => "Wyrmblood",
+    "BF4BAAA" => "Oldgate",
+    "BEsBAAA" => "Helliot",
+    "BP0BAAA" => "Behem"
+  );
+  foreach ($items as $key => $value) {
+    echo '
+    <tr>
+      <td>
+        <input type="checkbox" id="'.$key.'" name="[&'.$key.'=]" onchange="resultWrite()" checked>
+        <label class="checkbox" for="'.$key.'"></label>
+      </td>
+      <td>
+        <label class="text" for="'.$key.'">'.$value.'</label>
+      </td>
+    </tr>';
+    //echo " $key => Érték: $value<br>";
+  }
+?>
